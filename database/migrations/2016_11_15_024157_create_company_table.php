0798efbf5c01b06150d52e6ebbb6bd2335e3a990
edabascal <edabascal@gmail.com>
2016-11-15 12:02:45 -0300

    database error safe

--- database/migrations/2016_11_15_024157_create_company_table.php
+++ database/migrations/2016_11_15_024157_create_company_table.php
@@ -18,7 +18,6 @@ class CreateCompanyTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->increments('id');
             $table->timestamps();
         });
     }
