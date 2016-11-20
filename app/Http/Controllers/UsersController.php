0d4bb82025ad836d0c77aa5925d4a1e8dde3a7c4
Bengue85 <mlaura.benguerrero@gmail.com>
2016-11-20 20:49:00 -0300

    Cree controlador user

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index ()
+    {
+
+    }
+    public function create ()
+    {
+        return view('admin.users.create');
+    }
+    public function store (request $request)
+    {
+
+    }
+    public function show ($id)
+    {
+
+    }
+    public function edit ($id)
+    {
+
+    }
+    public function update(request $request, $id)
+    {
+
+    }
+    public function destroy ($id)
+  {
+
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+  route::group (['prefix'=>'admin'], function(){
+    route::resource('users','UsersController');
+  });
